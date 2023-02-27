2993294eafbe7f4b0d3b735bccad8c09410b4dd7
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-02-27 14:47:10 +0600

    update

---
+++ Basic_Java/Pattern4.java
@@ -0,0 +1,20 @@
+package Basic_Java;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter How many row : ");
+        int x = input.nextInt();
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j % 2 == 0)
+                    System.out.print("0  ");
+                else
+                    System.out.print("1  ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
